18e4de971c3bb2c36af4ecc8b0e974908304ac15
ryeen12 <152992632+ryeen12@users.noreply.github.com>
2025-03-07 01:41:00 +0300

    U ghost

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "LAKA-MD=A8RwASQb#oAdvTR2rO911A-xxbkFTOgfcJYNNWrlI77_uT_4hpqk",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=E04A0BpQ#XOfIlKfkCfNWlfFq2fT34-JdMvBMn9_sgtnGv2lnG8M",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
